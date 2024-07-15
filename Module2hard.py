86436266c8837bf33eac279757cbdb4098ae9b9d
Maximovex <maximove81@gmail.com>
2024-07-15 17:14:01 +0300

    Add files via upload

---
+++ Module2hard.py
@@ -0,0 +1,23 @@
+def check_unique(pair,matrix):
+    exist=False
+    for i in range(len(matrix)):
+        if (pair[0]==matrix[i][1] and pair[1]==matrix[i][0]):
+            exist=True
+    return exist
+pass_code=[]
+code_number=int(input('Кодовая цифра: '))
+if 3<=code_number<=20:
+    for i in range(1,code_number):
+        for j in range(1,code_number):
+            if code_number%(i+j)==0:
+                un_pair=[i,j]
+                check=check_unique(un_pair,pass_code)
+                if check==False and i!=j:
+                    pass_code.append(un_pair)
+            else:
+                continue
+else:
+    print('Число должно быть в диапазоне от 3 до 20')
+for i in range(len(pass_code)):
+    pass_code[i]=str(pass_code[i][0])+str(pass_code[i][1])
+print(*pass_code)
